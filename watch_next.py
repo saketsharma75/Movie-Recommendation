fda3aa6e6f8fca89f2004a630c918f3d37b57d72
saketsharma75 <126008772+saketsharma75@users.noreply.github.com>
2023-02-22 13:24:20 +0000

    Updated version
    Updated version with 3 movie recommendations insted of

--- watch_next.py
+++ watch_next.py
@@ -11,23 +11,18 @@ their world or destroy it? When the Hulk becomes too dangerous for the
 Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a
 planet where the Hulk can live in peace. Unfortunately, Hulk land on the
 planet Sakaar where he is sold into slavery and trained as a gladiator.”'''
+
+
 def watch_next(movie):
-    # create an empty list to store the movies with the highest similarity scores
-    similar_movies = []
-    # create a variable to store the highest similarity score
-    highest_similarity = 0
-    # create a variable to store the highest similarity score
+    similar_movies = [] # Create an empty list to store the similar movies
+    highest_similarity = 0.8 # Set the similarity threshold
     for movie_to_compare in movies:
-        # calculate the similarity between the movie description and the movie to compare
-        similarity = nlp(movie).similarity(nlp(movie_to_compare))
-        # if the similarity is higher than the highest similarity score
-        if similarity > highest_similarity:
-            # set the highest similarity score to the new similarity score
-            highest_similarity = similarity
-            # add the movie to the list of similar movies
+        similarity = nlp(movie).similarity(nlp(movie_to_compare))        
+        if similarity > highest_similarity:            
             similar_movies.append(movie_to_compare)
-    # return the list of similar movies
-    return similar_movies
+    similar_movies.sort(key=lambda x: nlp(x).similarity(nlp(movie)), reverse=True) # Sort the list by similarity    
+    return similar_movies[:3] # Return the top 3 similar movies
+
 
 
 # Create variable to store the movie description
@@ -39,7 +34,6 @@ planet Sakaar where he is sold into slavery and trained as a gladiator.'''
 
 # Call the function with the variable movie and print the result in a readable format
 next_movie = watch_next(movie)
-print()
 print("The movies you should watch next are:")
 print()
 for movie in next_movie:
